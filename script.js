2bd2696f4285a8c8385f5358c7986a0aa913a1d9
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-05-19 19:14:52 +0200

    initial commit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ fetch("http://www.splashbase.co/api/v1/images/search?query=laptop" )
 .then((response) => response.json())
 .then((laptop) => {
     console.log(laptop);
+    
+
     let cardImage = document.querySelectorAll('.card img')
     let smallString = document.querySelectorAll('.card small')
     let viewButton = document.querySelectorAll('.card button:first-of-type')
@@ -11,6 +13,7 @@ fetch("http://www.splashbase.co/api/v1/images/search?query=laptop" )
     let modalBody = document.querySelector('.modal-body')
     console.log(cardImage[0]);
     for(let i=0; i<cardImage.length; i++){
+        document.querySelectorAll(".card-img-top")[i].style.display = "none"
         let id = laptop.images[i].id
         EditButton[i].innerText ="Hide"
         cardImage[i].classList.add('img-fluig')
@@ -24,12 +27,12 @@ fetch("http://www.splashbase.co/api/v1/images/search?query=laptop" )
            let currentId =  e.currentTarget
            let currentCard = currentId.closest('.card')
            let currentCardImage = currentCard.querySelector('img')
-
+           
           /*  console.log(e);
            console.log(e.target.id, currentId)
            let currentImg = laptop.images.filter((laptop_image)=> laptop_image === currentId)
            currentImg = currentImg.url */
-           modalImg.src ="" 
+           modalImg.src = "" 
            modalImg.src = currentCardImage.src 
            
         })
@@ -54,13 +57,15 @@ fetch("http://www.splashbase.co/api/v1/images/search?query=laptop" )
 }
 
 const flowerImg = function(){
-    fetch("http://www.splashbase.co/api/v1/images/search?query=flower" )
+    let search = document.querySelector(".search-input").value;
+    if(search.length != 0){
+    fetch(`http://www.splashbase.co/api/v1/images/search?query="+${search}`)
     .then((response) => response.json())
     .then((flower) => {
         console.log(flower);
         for(let i = 0; i< 10; i++){
             let image = flower.images[i]
-
+            document.querySelectorAll('.card .flower-image')[i].style.display = "none"
             let img = document.createElement("img");
             let container = document.querySelectorAll(".image-conatiner")
             let cards = document.querySelectorAll(".card");
@@ -73,9 +78,19 @@ const flowerImg = function(){
             document.querySelectorAll(".card-img-top")[i].style.display = "none"
             
             // cards[i].appendChild("container");
-            
-            
+       
+           
         }
+          
+    })
+    .catch(err => {
+        console.error(err);
+    })
+}
+else {
+    fetch("http://www.splashbase.co/api/v1/images/search?query=flower")
+    .then((response) => response.json())
+    .then((flower) => {
         let cardImage = document.querySelectorAll('.card img')
         let smallString = document.querySelectorAll('.card small')
         let viewButton = document.querySelectorAll('.card button:first-of-type')
@@ -83,11 +98,13 @@ const flowerImg = function(){
         let modalBody = document.querySelector('.modal-body')
         console.log(viewButton[0]);
            for(let i=0; i<cardImage.length; i++){
+            
             let id = flower.images[i].id
             EditButton[i].innerText ="Hide"
             //console.log("id ", card_id)
-             cardImage[i].classList.add('img-fluig')
+             cardImage[i].classList.add('img-fluig', 'flower-image')
              cardImage[i].src = flower.images[i].url
+             document.querySelectorAll(".card-img-top")[i].style.display = "none"
              viewButton[i].setAttribute("data-toggle","modal")
              viewButton[i].setAttribute("data-target", "#exampleModal")
              viewButton[i].setAttribute("id", `${flower.images[i].id}`)       
@@ -113,13 +130,19 @@ const flowerImg = function(){
              modalImg.src = currentImg */
              
              smallString[i].innerText = flower.images[i].id 
-    }    
+             
+            
+            // cards[i].appendChild("container");
+       
+           
+        }
+          
     })
     .catch(err => {
         console.error(err);
     })
 }
-
+}
 
 
 
