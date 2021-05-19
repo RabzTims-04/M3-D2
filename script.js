87c6c6c2e93b2934e9ec3360e0786147e51d4218
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-05-19 17:05:36 +0200

    hotfix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const loadSecondary = () =>{
-    const cards = document.querySelector(".card");
+   
     let search = document.querySelector(".search-input").value;
     if(search.length != 0){
         fetch(`http://www.splashbase.co/api/v1/images/search?query="+${search}`)
@@ -7,17 +7,28 @@ const loadSecondary = () =>{
         .then((images)=>{
             console.log(images);
             // console.log(images.images.length);
-            for(let i = 0; i< images.images.length; i++){
-                const image = images.images[i]
-                const img = document.createElement("img");
-                img.classList.add("img-fluid")
+            for(let i = 0; i< 10; i++){
+                let image = images.images[i]
+
+                let img = document.createElement("img");
+                let container = document.querySelectorAll(".image-conatiner")
+                let cards = document.querySelectorAll(".card");
+                
+                img.classList.add("img-fluid");
                 img.src  = image.url;
 
-                cards.appendChild(img);
+                container[i].appendChild(img);
+                document.querySelectorAll(".card-img-top")[i].style.display = "none"
+                // cards[i].appendChild("container");
+                
+                
             }
 
         })
+        search = " "
     }
+   
+   
 }
 
 
