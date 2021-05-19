69176780b9555b7a736c96a304b7b629ec6e9c57
Rabz92 <rabbiailyas92@gmail.com>
2021-05-19 18:13:06 +0200

    merged

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
 
-const flowerImg = () =>{
-    let search = document.querySelector(".search-input").value;
-    if(search.length != 0){
-        fetch(`http://www.splashbase.co/api/v1/images/search?query="+${search}`)
-        .then((response) => response.json())
-        .then((images)=>{
-            console.log(images);
-            // console.log(images.images.length);
-            for(let i = 0; i< 10; i++){
-                let image = images.images[i]
-
-                let img = document.createElement("img");
-                let container = document.querySelectorAll(".image-conatiner")
-                let cards = document.querySelectorAll(".card");
-                
-                img.classList.add("img-fluid");
-                img.src  = image.url;
-                
-                container[i].innerHTML = " "
-                container[i].appendChild(img);
-                document.querySelectorAll(".card-img-top")[i].style.display = "none"
-                
-                // cards[i].appendChild("container");
-                
-                
-            }
-          
-
-        }).catch((err)=>{
-
-        })
-    }
-    
-}
-
-
-
 const laptopImg = function(){
 fetch("http://www.splashbase.co/api/v1/images/search?query=laptop" )
 .then((response) => response.json())
@@ -95,6 +58,24 @@ const flowerImg = function(){
     .then((response) => response.json())
     .then((flower) => {
         console.log(flower);
+        for(let i = 0; i< 10; i++){
+            let image = flower.images[i]
+
+            let img = document.createElement("img");
+            let container = document.querySelectorAll(".image-conatiner")
+            let cards = document.querySelectorAll(".card");
+            
+            img.classList.add("img-fluid");
+            img.src  = image.url;
+            
+            container[i].innerHTML = " "
+            container[i].appendChild(img);
+            document.querySelectorAll(".card-img-top")[i].style.display = "none"
+            
+            // cards[i].appendChild("container");
+            
+            
+        }
         let cardImage = document.querySelectorAll('.card img')
         let smallString = document.querySelectorAll('.card small')
         let viewButton = document.querySelectorAll('.card button:first-of-type')
